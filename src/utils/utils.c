8ee7733b65df0e0de8cbf1a671613a2a01749cb6
Loan Besse <loan.besse@epitech.eu>
2026-04-10 19:33:36 +0200

    {+} Added the my_getline function which essentially re-enables getline so that it works with enable_raw_mode & disable_raw_mode

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -31,3 +31,49 @@ void free_command_list(command_t *head)
         head = tmp;
     }
 }
+
+static int handle_backspace(int *i, char c)
+{
+    if (c != 127)
+        return 0;
+    if (*i > 0) {
+        (*i)--;
+        write(1, "\b \b", 3);
+    }
+    return 1;
+}
+
+static int handle_regular_char(char *buffer, int *i, char c)
+{
+    if (c == '\n' || c == '\r') {
+        write(1, "\n", 1);
+        buffer[*i] = '\0';
+        return 1;
+    }
+    buffer[*i] = c;
+    (*i)++;
+    write(1, &c, 1);
+    return 0;
+}
+
+char *my_getline(void)
+{
+    char *buffer = malloc(sizeof(char) * 1024);
+    int i = 0;
+    char c;
+    int txt = 0;
+
+    if (buffer == NULL)
+        return NULL;
+    while (1) {
+        txt = read(0, &c, 1);
+        if (txt <= 0 || c == 4) {
+            free(buffer);
+            return NULL;
+        }
+        if (handle_backspace(&i, c) == 1)
+            continue;
+        if (handle_regular_char(buffer, &i, c) == 1)
+            return buffer;
+    }
+}
