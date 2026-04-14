42334fe4e36f440657b804a9c0a403e146655740
silvucani <silvucani12@gmail.com>
2026-04-14 20:18:37 +0200

    {+} feat(history): plug history store into mysh main loop

--- src/core/mysh.c
+++ src/core/mysh.c
@@ -7,7 +7,7 @@
 #define _GNU_SOURCE
 #include "../../include/my.h"
 
-static int handle_input(char *line, char **env, env_t **env_list)
+static int handle_input(char *line, mysh_t *shell, char **env)
 {
     command_t *cmd_list;
 
@@ -21,9 +21,10 @@ static int handle_input(char *line, char **env, env_t **env_list)
         return 1;
     }
     if (line[0] != '\0') {
+        history_add(shell->history, line);
         cmd_list = create_command_list(line);
         if (cmd_list != NULL) {
-            exec_command(cmd_list, env, env_list);
+            exec_command(cmd_list, env, &shell->env);
             free_command_list(cmd_list);
         }
     }
@@ -36,18 +37,20 @@ int mysh(char **env)
     mysh_t shell;
 
     shell.env = env_to_list(env);
+    shell.history = history_init(500);
     enable_raw_mode(&shell.original_term);
     while (1) {
         if (isatty(0))
             my_putstr("$> ");
         line = my_getline();
-        if (handle_input(line, env, &shell.env) == 1)
+        if (handle_input(line, &shell, env) == 1)
             break;
         free(line);
     }
     disable_raw_mode(&shell.original_term);
     if (line != NULL)
         free(line);
+    history_free(shell.history);
     free_env_list(shell.env);
     return 0;
 }
