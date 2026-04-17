033fb5626973c6ba6769ee1931494333c6643e13
Loan Besse <loan.besse@epitech.eu>
2026-04-17 17:37:24 +0200

    {-} function removal for the banana standard

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,45 +6,6 @@
 */
 #include "../../include/my.h"
 
-void free_tab(char **tab)
-{
-    int i = 0;
-
-    if (tab == NULL)
-        return;
-    while (tab[i] != NULL) {
-        free(tab[i]);
-        i++;
-    }
-    free(tab);
-}
-
-char *my_getenv(env_t *env, char *name)
-{
-    env_t *curr = env;
-
-    while (curr != NULL) {
-        if (my_strcmp(curr->name, name) == 0)
-            return curr->value;
-        curr = curr->next;
-    }
-    return NULL;
-}
-
-void free_command_list(command_t *head)
-{
-    command_t *tmp;
-
-    while (head != NULL) {
-        tmp = head->next;
-        free_tab(head->args);
-        free(head->in_file);
-        free(head->out_file);
-        free(head);
-        head = tmp;
-    }
-}
-
 static void redraw_after_backspace(char *buffer, line_state_t *state)
 {
     int tmp = 0;
@@ -191,25 +152,4 @@ char *my_getline(history_t *hist)
             return buffer;
         }
     }
-}
-
-char **tokens_to_array(token_t *head)
-{
-    int count = 0;
-    char **args;
-    int i = 0;
-    token_t *curr = head;
-
-    for (; curr != NULL; curr = curr->next)
-        count++;
-    args = malloc(sizeof(char *) * (count + 1));
-    if (args == NULL)
-        return NULL;
-    curr = head;
-    for (; curr != NULL; curr = curr->next) {
-        args[i] = my_strdup(curr->value);
-        i++;
-    }
-    args[i] = NULL;
-    return args;
 }
