48009cc168bb8b4a565831326255f71d9f7934e1
Matt <matthis.roda@epitech.eu>
2026-04-21 13:42:36 +0200

    {+} add redraw_prompt_line function to update the prompt line after completion or menu close

--- src/completion/completion_draw.c
+++ src/completion/completion_draw.c
@@ -5,3 +5,17 @@
 ** redraw the prompt line after a completion or menu close
 */
 #include "../../include/my.h"
+
+void redraw_prompt_line(char *buffer, line_state_t *st)
+{
+    int n = st->max_len - st->i;
+
+    if (!st->interactive)
+        return;
+    write(1, "\r\033[K$> ", 7);
+    write(1, buffer, st->max_len);
+    while (n > 0) {
+        write(1, "\b", 1);
+        n--;
+    }
+}
