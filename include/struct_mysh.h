79b1d457f834c1b7cca2e01201323568f10ae54a
Loan Besse <loan.besse@epitech.eu>
2026-04-21 15:27:12 +0200

    {-} removal of old, unused structures

--- include/struct_mysh.h
+++ include/struct_mysh.h
@@ -51,30 +51,6 @@ typedef struct env_s {
     struct env_s *next;
 } env_t;
 
-typedef enum separator_e {
-    SEP_NONE,
-    SEP_SEMI,
-    SEP_PIPE
-} separator_t;
-
-typedef enum redir_e {
-    REDIR_NONE,
-    REDIR_RIGHT,
-    REDIR_DOUBLE_RIGHT,
-    REDIR_LEFT,
-    REDIR_DOUBLE_LEFT
-} redir_t;
-
-typedef struct command_s {
-    char **args;
-    redir_t in_type;
-    char *in_file;
-    redir_t out_type;
-    char *out_file;
-    separator_t separator;
-    struct command_s *next;
-} command_t;
-
 typedef struct history_entry_s {
     char *line;
     struct history_entry_s *next;
@@ -107,10 +83,5 @@ typedef struct mysh_s {
     struct termios original_term;
     history_t *history;
 } mysh_t;
-
-typedef struct pipe_ctx_s {
-    char **env;
-    struct env_s *env_list;
-} pipe_ctx_t;
 
 #endif
