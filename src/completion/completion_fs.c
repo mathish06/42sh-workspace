333fa5d46c6a1448630c64dc22f032d34442f41d
Matt <matthis.roda@epitech.eu>
2026-04-21 13:34:43 +0200

    {+} add is_directory function to check if a path is a directory

--- src/completion/completion_fs.c
+++ src/completion/completion_fs.c
@@ -5,3 +5,22 @@
 ** small filesystem helpers used by the completion feature
 */
 #include "../../include/my.h"
+
+int is_directory(const char *dir, const char *name)
+{
+    char *full;
+    struct stat st;
+    int len;
+    int rc;
+
+    len = my_strlen((char *)dir) + my_strlen((char *)name) + 2;
+    full = malloc(sizeof(char) * len);
+    if (full == NULL)
+        return 0;
+    my_strcpy(full, (char *)dir);
+    my_strcat(full, "/");
+    my_strcat(full, (char *)name);
+    rc = stat(full, &st);
+    free(full);
+    return (rc == 0 && S_ISDIR(st.st_mode));
+}
