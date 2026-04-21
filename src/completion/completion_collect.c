9a43f939cc1884f9e5420223b02e2866ed0623e7
Matt <matthis.roda@epitech.eu>
2026-04-21 13:46:57 +0200

    {+} add starts_with function to check if a string starts with a given prefix

--- src/completion/completion_collect.c
+++ src/completion/completion_collect.c
@@ -5,3 +5,15 @@
 ** read a directory and collect entries matching a prefix
 */
 #include "../../include/my.h"
+
+int starts_with(const char *str, const char *pre)
+{
+    int i = 0;
+
+    while (pre[i] != '\0') {
+        if (str[i] != pre[i])
+            return 0;
+        i++;
+    }
+    return 1;
+}
