f66ff5dd349371ce2e29bc9004ce9e790a2c2abe
Loan Besse <loan.besse@epitech.eu>
2026-04-21 16:18:19 +0200

    {+} Added the "free_alias_list" function which allows freeing the alias_t structure

---
+++ src/alias/alias_tools.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2026
+** alias_tools.c
+** File description:
+** alias_tools for the 42sh
+*/
+#include "../../include/my.h"
+
+void free_alias_list(alias_t *head)
+{
+    alias_t *curr = head;
+    alias_t *next_node;
+
+    while (curr != NULL) {
+        next_node = curr->next;
+        if (curr->value != NULL)
+            free(curr->value);
+        if (curr->name != NULL)
+            free(curr->name);
+        free(curr);
+        curr = next_node;
+    }
+}
