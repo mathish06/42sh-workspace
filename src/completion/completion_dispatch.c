b8d9535721b3340014bb750e31ea87b2e6d142c7
Matt <matthis.roda@epitech.eu>
2026-04-23 16:02:38 +0200

    {+} add extend_prefix function to handle prefix extension logic in completion

--- src/completion/completion_dispatch.c
+++ src/completion/completion_dispatch.c
@@ -11,3 +11,16 @@ int single_match(line_ctx_t *lc, comp_ctx_t *ctx, comp_list_t *l)
     insert_completion(lc, ctx, l->entries[0], l->is_dir[0]);
     return 0;
 }
+
+int extend_prefix(line_ctx_t *lc, comp_ctx_t *ctx, comp_list_t *l)
+{
+    char *cp = common_prefix(l);
+    int extended = 0;
+
+    if (my_strlen(cp) > my_strlen(ctx->prefix)) {
+        insert_completion(lc, ctx, cp, 0);
+        extended = 1;
+    }
+    free(cp);
+    return extended;
+}
