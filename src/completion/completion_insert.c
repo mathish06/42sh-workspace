b3eed1bde573389b17ea1a4c87b72be8be425a56
Matt <matthis.roda@epitech.eu>
2026-04-29 11:33:24 +0200

    implement split_word function to parse directory and prefix from input word

--- src/completion/completion_insert.c
+++ src/completion/completion_insert.c
@@ -12,3 +12,21 @@ static char *extract_word(char *buffer, int start, int end)
         return my_strdup("");
     return my_strndup(buffer + start, end - start);
 }
+
+static void split_word(comp_ctx_t *ctx, char *word)
+{
+    int i = my_strlen(word) - 1;
+
+    while (i >= 0 && word[i] != '/')
+        i--;
+    if (i < 0) {
+        ctx->dir = my_strdup(".");
+        ctx->prefix = my_strdup(word);
+        return;
+    }
+    if (i == 0)
+        ctx->dir = my_strdup("/");
+    else
+        ctx->dir = my_strndup(word, i);
+    ctx->prefix = my_strdup(word + i + 1);
+}
