36c8858e691c25c08c0c58017387735cfa447498
Loan Besse <loan.besse@epitech.eu>
2026-04-30 11:36:06 +0200

    {~} transfer of the functions "find_operator" + "find_any_redir" + "create_leaf_node" to a new file

--- src/parsing/tree.c
+++ src/parsing/tree.c
@@ -6,52 +6,6 @@
 */
 #include "../../include/my.h"
 
-static token_t *find_operator(token_t *head, token_type_t type)
-{
-    token_t *curr = head;
-    int depth = 0;
-    token_t *last_found = NULL;
-
-    while (curr != NULL) {
-        if (curr->type == TOKEN_PAREN_LEFT)
-            depth++;
-        if (curr->type == TOKEN_PAREN_RIGHT)
-            depth--;
-        if (depth == 0 && curr->type == type)
-            last_found = curr;
-        curr = curr->next;
-    }
-    return last_found;
-}
-
-static token_t *find_any_redir(token_t *head)
-{
-    token_t *split = NULL;
-
-    split = find_operator(head, TOKEN_REDIR_RIGHT);
-    if (split == NULL)
-        split = find_operator(head, TOKEN_REDIR_DOUBLE_RIGHT);
-    if (split == NULL)
-        split = find_operator(head, TOKEN_REDIR_LEFT);
-    if (split == NULL)
-        split = find_operator(head, TOKEN_REDIR_DOUBLE_LEFT);
-    return split;
-}
-
-static ast_node_t *create_leaf_node(token_t *head)
-{
-    ast_node_t *node = malloc(sizeof(ast_node_t));
-
-    if (node == NULL)
-        return NULL;
-    node->type = NODE_COMMAND;
-    node->left = NULL;
-    node->right = NULL;
-    node->args = tokens_to_array(head);
-    free_tokens(head);
-    return node;
-}
-
 static void assign_node_type(ast_node_t *node, token_type_t type)
 {
     if (type == TOKEN_SEPARATOR)
