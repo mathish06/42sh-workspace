3eb4533ed98d55d5de0b3af278c1c0a139bc79ef
Matt <matthis.roda@epitech.eu>
2026-05-02 19:10:05 +0200

    {+} add resize_word function to manage word length adjustments in completion buffer

--- src/completion/completion_splice.c
+++ src/completion/completion_splice.c
@@ -25,3 +25,22 @@ static void shift_left(char *buffer, int from, int max, int by)
         p++;
     }
 }
+
+static void resize_word(char *buffer, line_state_t *st,
+    comp_ctx_t *ctx, int new_len)
+{
+    int old_len = ctx->word_end - ctx->word_start;
+    int diff;
+
+    if (new_len > old_len) {
+        diff = new_len - old_len;
+        shift_right(buffer, ctx->word_end, st->max_len, diff);
+        st->max_len += diff;
+    }
+    if (new_len < old_len) {
+        diff = old_len - new_len;
+        shift_left(buffer, ctx->word_end - diff, st->max_len, diff);
+        st->max_len -= diff;
+    }
+    ctx->word_end = ctx->word_start + new_len;
+}
