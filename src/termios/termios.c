66fe19422964c01e5617fd90bfad298997fa5dde
silvucani <silvucani12@gmail.com>
2026-05-04 09:22:47 +0200

    {~} fix: zero-init termios structs to avoid uninitialized padding bytes in tcsetattr

--- src/termios/termios.c
+++ src/termios/termios.c
@@ -4,12 +4,15 @@
 ** File description:
 ** termios for the 42sh
 */
+#include <string.h>
 #include "../../include/my.h"
 
 void enable_raw_mode(struct termios *original_term)
 {
     struct termios raw;
 
+    memset(original_term, 0, sizeof(*original_term));
+    memset(&raw, 0, sizeof(raw));
     tcgetattr(STDIN_FILENO, original_term);
     raw = *original_term;
     raw.c_lflag &= ~(ECHO | ICANON);
