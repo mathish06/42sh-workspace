14421d65ba4cab986f6d90b408da87ee24c9104d
Loan Besse <loan.besse@epitech.eu>
2026-05-05 11:42:07 +0200

    {~} Enhanced security + addition of a period ('.') to the end of `strerror` + display on error outputs

--- src/builtins/my_cd.c
+++ src/builtins/my_cd.c
@@ -19,13 +19,19 @@ static void update_env_var(mysh_t *shell, char *key, char *value)
 
 static int handle_chdir_error(char *path, char *old)
 {
-    if (errno == ENOTDIR) {
-        my_putstr(path);
-        write(2, ": Not a directory.\n", 19);
-    } else {
-        perror(path);
-    }
+    my_puterr(path);
     free(old);
+    if (errno == ENOTDIR) {
+        my_puterr(": Not a directory.\n");
+        return 1;
+    }
+    if (errno == EACCES) {
+        my_puterr(": Permission denied.\n");
+        return 1;
+    }
+    my_puterr(": ");
+    my_puterr(strerror(errno));
+    my_puterr(".\n");
     return 1;
 }
 
@@ -51,7 +57,7 @@ static int handle_cd_home(mysh_t *shell)
     char *home = my_getenv(shell->env, "HOME");
 
     if (home == NULL) {
-        my_putstr("cd: No home directory.\n");
+        my_puterr("cd: No home directory.\n");
         return 1;
     }
     return change_directory(home, shell);
@@ -62,7 +68,7 @@ static int handle_cd_dash(mysh_t *shell)
     char *oldpwd = my_getenv(shell->env, "OLDPWD");
 
     if (oldpwd == NULL) {
-        my_putstr(": No such file or directory.\n");
+        my_puterr(": No such file or directory.\n");
         return 1;
     }
     return change_directory(oldpwd, shell);
@@ -71,7 +77,7 @@ static int handle_cd_dash(mysh_t *shell)
 int my_cd(char **args, mysh_t *shell)
 {
     if (args[1] != NULL && args[2] != NULL) {
-        my_putstr("cd: Too many arguments.\n");
+        my_puterr("cd: Too many arguments.\n");
         return 1;
     }
     if (args[1] == NULL || my_strcmp(args[1], "~") == 0)
