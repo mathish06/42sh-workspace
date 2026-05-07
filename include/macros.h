1f37615cc37bf5250a07b55a1a9abbdbb2b58b29
mathis.haba <mathis1.haba@epitech.eu>
2026-05-07 11:59:38 +0200

    {~} align macro definitions for better readability

--- include/macros.h
+++ include/macros.h
@@ -8,12 +8,12 @@
 #ifndef MACROS_H
     #define MACROS_H
 
-#define RESET     "\033[0m"
+    #define RESET "\033[0m"
 
-#define ROUGE     "\033[31m"
-#define VERT      "\033[32m"
-#define BLEU      "\033[34m"
-#define CYAN      "\033[36m"
-#define MAGENTA   "\033[35m"
+    #define ROUGE "\033[31m"
+    #define VERT "\033[32m"
+    #define BLEU "\033[34m"
+    #define CYAN "\033[36m"
+    #define MAGENTA "\033[35m"
 
 #endif
